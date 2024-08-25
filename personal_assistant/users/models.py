0316faab1e5bd60f88057ce8b661d510f10a97e0
Regina0101 <ritodo1010@gmail.com>
2024-08-25 10:04:14 +0100

    Change default value in is_active variable to False

--- personal_assistant/users/models.py
+++ personal_assistant/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = models.CharField(max_length=255, blank=False)
     email = models.EmailField(unique=True, blank=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     group_id = models.CharField(max_length=10, choices=[('admin', 'Admin'), ('user', 'User')])
     avatar_url = models.URLField(blank=True, null=True)
 
