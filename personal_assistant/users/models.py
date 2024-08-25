6ff7338ad520d80d57e09705c1a1833e9bd083c7
PYOrm <ormagedon2007@gmail.com>
2024-08-25 16:58:09 +0300

    Base Template

--- personal_assistant/users/models.py
+++ personal_assistant/users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     username = models.CharField(max_length=255, blank=False)
     email = models.EmailField(unique=True, blank=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     group_id = models.CharField(max_length=10, choices=[('admin', 'Admin'), ('user', 'User')])
     avatar_url = models.URLField(blank=True, null=True)
 
