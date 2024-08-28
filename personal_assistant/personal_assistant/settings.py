fd56254b1a33644e8e9643346295a0038a41917c
AlexMois77 <159935651+AlexMois77@users.noreply.github.com>
2024-08-28 18:04:38 +0200

    Add autorization for notes

--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -140,10 +140,10 @@ LOGIN_REDIRECT_URL = '/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.example.com'
-EMAIL_PORT = 465
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'testuser11@meta.ua'
-EMAIL_HOST_PASSWORD = 'Test1234'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- personal_assistant/personal_assistant/urls.py
+++ personal_assistant/personal_assistant/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("latest_news.urls")),
     path('users/', include("users.urls")),  # Виправлений шлях
-     path('notes/', include('notes.urls')),
+    path('notes/', include('notes.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('contacts/', include("contacts.urls")),  # Доданий шлях для додатку 'contacts'
+    path('contacts/', include("contacts.urls")),  # Доданий шлях для додатку 'contacts' - contactS чи contact
 ]
