1d4bcf0f47416c3da6857bbfbcb0f63bd3f731ca
Andriy <AndriySydor91@gmail.com>
2024-08-30 12:08:16 +0300

    Save changes on backup-branch before merging to contacts

---
+++ personal_assistant/notes/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Note, Tag
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['body_tag']
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['body', 'tags']
+        widgets = {
+            'tags': forms.SelectMultiple(attrs={'class': 'form-select'})  
+        }
+
+class TagFilterForm(forms.Form):
+    tag = forms.ModelChoiceField(
+        queryset=Tag.objects.all(),
+        required=False,
+        empty_label='All tags',
+        label='Filter by tag',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    
