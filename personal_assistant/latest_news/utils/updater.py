dce0d30353822f3d79f55128542cfa0019a15e8c
PYOrm <ormagedon2007@gmail.com>
2024-09-01 11:01:32 +0300

    fix templates and add migration

--- personal_assistant/latest_news/utils/updater.py
+++ personal_assistant/latest_news/utils/updater.py
@@ -12,8 +12,9 @@ def run_parsers():
 
 
 def start():
+    run_parsers()
     scheduler = BackgroundScheduler()
     scheduler.add_job(run_parsers, 'interval', minutes=1440)
     scheduler.start()
-    print("1")
+    # print("1")
 
