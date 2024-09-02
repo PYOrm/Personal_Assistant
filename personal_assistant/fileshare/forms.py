c0273c3088db1be2b85a510168034f0a6d8d9837
PYOrm <158809598+PYOrm@users.noreply.github.com>
2024-09-02 13:00:16 +0300

    Delete personal_assistant directory

--- personal_assistant/fileshare/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import File
-
-class FileUploadForm(forms.Form):
-    file = forms.FileField(required=True)
-    category = forms.ChoiceField(choices=File.CATEGORY_CHOICES, required=True)
-
-
