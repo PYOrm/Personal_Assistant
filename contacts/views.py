d99806f73838cb9f312f01ffee62e866b9223aea
PYOrm <158809598+PYOrm@users.noreply.github.com>
2024-09-02 13:31:16 +0300

    Add files via upload

---
+++ contacts/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import Contact
+from .forms import ContactForm
+from datetime import date, timedelta
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+
+@login_required
+def contact_list(request):
+    days = request.GET.get('days')
+    search_query = request.GET.get('search', '')
+
+    if days and days.isdigit():
+        days = int(days)
+    else:
+        days = None
+
+    today = timezone.now().date()
+    logger.info(f"Filtering contacts for the next {days} days from {today}")
+
+    contacts = Contact.objects.filter(owner=request.user)
+
+    if search_query:
+        contacts = contacts.filter(name__icontains=search_query)
+
+    if days:
+        filtered_contacts = []
+        for contact in contacts:
+            if contact.date:
+                contact_month_day = (contact.date.month, contact.date.day)
+                today_month_day = (today.month, today.day)
+                end_month_day = ((today + timedelta(days=days)).month, (today + timedelta(days=days)).day)
+
+                if today_month_day <= contact_month_day <= end_month_day or \
+                   (today_month_day <= contact_month_day and end_month_day < today_month_day) or \
+                   (end_month_day < today_month_day and contact_month_day <= end_month_day):
+                    filtered_contacts.append(contact)
+        contacts = filtered_contacts
+        logger.info(f"Found {len(filtered_contacts)} contacts")
+    else:
+        contacts = contacts.order_by('name')
+
+    
+    paginator = Paginator(contacts, 10)  
+    page_number = request.GET.get('page')  
+    page_obj = paginator.get_page(page_number)  
+
+    return render(request, 'contacts/contact_list.html', {
+        'contacts': page_obj,  
+        'page_obj': page_obj,  
+        'search_query': search_query,  
+    })
+def contact_detail(request, pk):
+
+    contact = get_object_or_404(Contact, pk=pk, owner=request.user)
+    return render(request, 'contacts/contact_detail.html', {'contact': contact})
+
+@login_required
+def contact_create(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
+            return redirect('contacts:contact_detail', pk=contact.pk) 
+
+    else:
+        form = ContactForm()
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+@login_required
+def contact_update(request, pk):
+    contact = get_object_or_404(Contact, pk=pk, owner=request.user)
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contacts:contact_detail', pk=contact.pk)
+        else:
+            print(form.errors)  # Виводить помилки форми, якщо вони є
+    else:
+        form = ContactForm(instance=contact)
+
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+@login_required
+def contact_delete(request, pk):
+    contact = get_object_or_404(Contact, pk=pk, owner=request.user)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contacts:contact_list')
+    return render(request, 'contacts/contact_confirm_delete.html', {'contact': contact})
+
+@login_required
+def upcoming_birthdays(request, days=7):
+    today = date.today()
+    
+    future_date = today + timedelta(days=days)
+    contacts = Contact.objects.filter(
+        date__gte=today,
+        date__lte=future_date,
+        owner=request.user 
+    ).order_by('date')
+
+    return render(request, 'contacts/upcoming_birthdays.html', {'contacts': contacts, 'days': days})
---
+++ notes/views.py
@@ -0,0 +1,81 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from .models import Note, Tag
+from .forms import NoteForm, TagForm, TagFilterForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class NotesView(LoginRequiredMixin, ListView):
+    login_url = "/users/login/"
+    model = Note
+    template_name = 'notes/notes.html'
+    context_object_name = 'notes'
+    paginate_by = 6
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        tag = self.request.GET.get('tag')
+        search_query = self.request.GET.get('search')
+        user = self.request.user
+        
+        # Фільтрація за користувачем
+        queryset = queryset.filter(user=user)
+        
+        if tag:
+            queryset = queryset.filter(tags__id=tag)
+        
+        if search_query:
+            queryset = queryset.filter(title__icontains=search_query)
+        
+        return queryset.order_by('title')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TagFilterForm(self.request.GET)
+        context['search_query'] = self.request.GET.get('search', '')
+        return context
+
+class AddTagView(LoginRequiredMixin, CreateView):
+    login_url = "/users/login/"
+    model = Tag
+    template_name = 'notes/create_tag.html'
+    success_url = reverse_lazy('notes:create_tag')
+    form_class = TagForm
+
+class AddNoteView(LoginRequiredMixin, CreateView):
+    login_url = "/users/login/"
+    model = Note
+    template_name = 'notes/create_note.html'
+    success_url = reverse_lazy('notes:notes')
+    form_class = NoteForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class EditNoteView(LoginRequiredMixin, UpdateView):
+    login_url = "/users/login/"
+    model = Note
+    template_name = 'notes/edit_note.html'
+    success_url = reverse_lazy('notes:notes')
+    form_class = NoteForm
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
+
+class DeleteNoteView(LoginRequiredMixin, DeleteView):
+    login_url = "/users/login/"
+    model = Note
+    template_name = 'notes/delete_note.html'
+    success_url = reverse_lazy('notes:notes')
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
+
+class NoteDetailView(LoginRequiredMixin, DetailView):
+    login_url = "/users/login/"
+    model = Note
+    template_name = 'notes/detail_note.html'
+    context_object_name = 'note'
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
