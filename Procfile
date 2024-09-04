4e29f26cb482da4c196a622e87535d31f6f34f70
Regina0101 <ritodo1010@gmail.com>
2024-09-04 11:30:55 +0100

    create migrations/requirements.txt/Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Personal_Assistant.wsgi --bind 0.0.0.0:8000
--- personal_assistant/settings.py
+++ personal_assistant/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
