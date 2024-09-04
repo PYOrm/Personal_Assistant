45b6be391e5ed79b2a7f6fe35cc1b59d6c003f86
Regina0101 <ritodo1010@gmail.com>
2024-09-04 15:39:42 +0100

    hide key

--- personal_assistant/settings.py
+++ personal_assistant/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c#4sm0yyn(r9gxpv3j7_@^dl(z2q2+uuke0=xz5**c%tjsvv#i'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
