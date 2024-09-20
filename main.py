7c12fdedb3d5c6c9e7f0916463344090f1dbd59b
AlexMois77 <159935651+AlexMois77@users.noreply.github.com>
2024-09-20 09:41:25 +0200

    Добавлены файлы и настроен перенос строк

---
+++ main.py
@@ -0,0 +1,19 @@
+from multiprocessing import Process
+import os 
+
+def runserver():
+    os.system('python manage.py runserver 0.0.0.0:8000')
+
+def process_tasks():
+    os.system('python manage.py process_tasks')
+
+if __name__ == '__main__':
+    processes=[
+        Process(target=runserver),
+        Process(target=process_tasks),        
+    ]
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
